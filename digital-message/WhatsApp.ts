f56e2482bcd61954a9570a2657598c429a8941f5
Jayden Ho <jinglong05@hotmail.com>
2022-09-30 20:42:15 +0800

    added feat to extract recipient id from CS

--- digital-message/WhatsApp.ts
+++ digital-message/WhatsApp.ts
@@ -1,13 +1,14 @@
 import fetch from 'node-fetch';
+import { isContext } from 'vm';
 import { MessageBirdResponse } from "./MessageBirdResponse";
 
-async function postWhatsApp(message, key): Promise<MessageBirdResponse> {
+async function postWhatsApp(message, key, contactID): Promise<MessageBirdResponse> {
     try {
       const response = await fetch('https://conversations.messagebird.com/v1/send',
       {
         method: 'POST',
         body: JSON.stringify({
-          to: '+6597820225',
+          to: '+65'+ contactID,
           type: 'hsm',
           from: '0d80abd0-6ab4-4b51-b91c-a034d7c62669',
           content:{
@@ -31,14 +32,19 @@ async function postWhatsApp(message, key): Promise<MessageBirdResponse> {
       });
   
       if (!response.ok) {
-        throw new Error(`Error! status: ${response.status}`);
+        throw new Error(`Error!!!!!`);
       }
   
       const result = await response.json();
       return result as MessageBirdResponse;
 
     } catch (err) {
-      console.log(err);
+      const result = {
+        "id": "",
+        "status": "Invalid Recipient ID",
+        "fallback": ""
+      };
+      return result as MessageBirdResponse;
     }
   }
 
--- digital-message/index.ts
+++ digital-message/index.ts
@@ -7,46 +7,64 @@ import { extractData } from "./DataExtractor";
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
 
     const { apiKey } = process.env;
-    context.log('HTTP trigger function processing a request.');
     
-    const data = extractData(req.body);
-    const message = data[0];
+    context.log('HTTP trigger function processing a request.');
+    // context.log(req.body);
 
-    // this is a list now
+    const data = extractData(req.body);
+
+    const message = data[0];
     const channelList = data[1];
+    const contactID = data[2];
     
     let mbResponse: MessageBirdResponse;
     const channelSucceeded = {}
     const channelFailed = {}
+    let code = 0;
 
     for (const channel of channelList) {
+
       if (channel == "WhatsApp"){
-        // if (req.body.channel == "WhatsApp"){
-          mbResponse = await postWhatsApp(message, apiKey);
+          mbResponse = await postWhatsApp(message, apiKey, contactID);
+          context.log(mbResponse);
         }
     
-      if (channel == "LINE"){
+      else if (channel == "LINE"){
         mbResponse = await postLine(message, apiKey);
       }
 
-      let tempDict = {}
-
       if( mbResponse["status"] == "accepted"){
         channelSucceeded[channel] = mbResponse["status"]
-      } else {
-                                 // insert error handling into this function
+      } 
+
+      else {
+        // insert error handling into this function
         channelFailed[channel] = mbResponse["status"]
       }
 
+      if (Object.keys(channelFailed).length === 0){
+        code = 200;
+      }
+      else{
+        code = 500;
+      }
+
+      // context.log(JSON.stringify({
+      //   "code": code,
+      //   "channels_sent": channelList,
+      //   "channel_succeeded": channelSucceeded,
+      //   "channel_failed": channelFailed,
+      // }));
+
       context.res = {
-          // status: 200, /* Defaults to 200 */
           body: (JSON.stringify({
-            "code": 200,
-            "channels_sent":channelList,
-            "channel_succeeded":channelSucceeded,
-            "channel_failed":channelFailed,
+            "code": code,
+            "channels_sent": channelList,
+            "channel_succeeded": channelSucceeded,
+            "channel_failed": channelFailed,
           }))
       };
+
     }
 };
 
