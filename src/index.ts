1ce6f304ea4d21cce357ac0a3dc517a0930f47af
kellylee99 <80463336+kellylee99@users.noreply.github.com>
2022-11-07 02:17:08 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,49 +13,49 @@ export async function makeRequest() {
   }
   
 export async function testWhatsappFunctionOne(){
-    var content = await messageContent('Spg0nt3GILwAxHbVVKlxoKoMm/123','6583685216','hi');
+    var content = await messageContent('OD9JjrVluF4ON00310kMWXJLa123','6583685216','hi');
     var value = content["status"]
     return value
 }
 
 export async function testWhatsappFunctionTwo(){
-    var content = await imageContent('Spg0nt3GILwAxHbVVKlxoKoMm123','6583685216','image/png','hi','https://eu-images.contentstack.com/v3/assets/blt2233c63cb81d9b74/blt5dc0dc4e42905d70/634c03d196c0500f0511b5f6/cat.png');
+    var content = await imageContent('OD9JjrVluF4ON00310kMWXJLa123','6583685216','image/png','hi','https://eu-images.contentstack.com/v3/assets/blt2233c63cb81d9b74/blt5dc0dc4e42905d70/634c03d196c0500f0511b5f6/cat.png');
     var value = content["status"]
     return value
 }
 
 export async function testWhatsappFunctionThree(){
-    var content = await videoContent('Spg0nt3GILwAxHbVVKlxoKoMm123','6583685216','Video','hi','https://eu-assets.contentstack.com/v3/assets/blt2233c63cb81d9b74/blt2bfc06495cdfb725/634a28328f37810f03d5b749/catVideo.mp4');
+    var content = await videoContent('OD9JjrVluF4ON00310kMWXJLa123','6583685216','Video','hi','https://eu-assets.contentstack.com/v3/assets/blt2233c63cb81d9b74/blt2bfc06495cdfb725/634a28328f37810f03d5b749/catVideo.mp4');
     var value = content["status"]
     return value
 }
 
 export async function testWhatsappFunctionFour(){
-    var content = await documentContent('Spg0nt3GILwAxHbVVKlxoKoMm123','6583685216','Document','hi','https://eu-assets.contentstack.com/v3/assets/blt2233c63cb81d9b74/bltab4224846f2fae19/634a2e67c473a75d5c5e76ea/Schroders_transcript.pdf');
+    var content = await documentContent('OD9JjrVluF4ON00310kMWXJLa123','6583685216','Document','hi','https://eu-assets.contentstack.com/v3/assets/blt2233c63cb81d9b74/bltab4224846f2fae19/634a2e67c473a75d5c5e76ea/Schroders_transcript.pdf');
     var value = content["status"]
     return value
 }
 
 export async function testLINEFunctionOne(){
-    var content = await line_message('Spg0nt3GILwAxHbVVKlxoKoMm','Uba3d02f75bbe48738a9c47926a2f173d/','hi');
+    var content = await line_message('OD9JjrVluF4ON00310kMWXJLa','Uba3d02f75bbe48738a9c47926a2f173d/','hi');
     var value = content["status"]
     return value
 }
 
 export async function testLINEFunctionTwo(){
-    var content = await line_image('Spg0nt3GILwAxHbVVKlxoKoMm','Uba3d02f75bbe48738a9c47926a2f173d/','image','https://eu-images.contentstack.com/v3/assets/blt2233c63cb81d9b74/bltd201a6de6c6cb58c/631f5165ad808074367e0a6f/olivia.png');
+    var content = await line_image('OD9JjrVluF4ON00310kMWXJLa','Uba3d02f75bbe48738a9c47926a2f173d/','image','https://eu-images.contentstack.com/v3/assets/blt2233c63cb81d9b74/bltd201a6de6c6cb58c/631f5165ad808074367e0a6f/olivia.png');
     var value = content["status"]
     return value
 }
 
 export async function testLINEFunctionThree(){
-    var content = await line_video('Spg0nt3GILwAxHbVVKlxoKoMm','Uba3d02f75bbe48738a9c47926a2f173d/','Video','https://eu-assets.contentstack.com/v3/assets/blt2233c63cb81d9b74/blt2bfc06495cdfb725/634a28328f37810f03d5b749/catVideo.mp4');
+    var content = await line_video('OD9JjrVluF4ON00310kMWXJLa','Uba3d02f75bbe48738a9c47926a2f173d/','Video','https://eu-assets.contentstack.com/v3/assets/blt2233c63cb81d9b74/blt2bfc06495cdfb725/634a28328f37810f03d5b749/catVideo.mp4');
     var value = content["status"]
     return value
 }
 
 export async function testcontact(){
-    var hi =  await retrieveContacts('Spg0nt3GILwAxHbVVKlxoKoMm',"Insurance");
+    var hi =  await retrieveContacts('OD9JjrVluF4ON00310kMWXJLa',"Insurance");
     var items = hi["items"]
     var loop = ""
     var dict = []
@@ -84,7 +84,7 @@ export async function testcontact(){
 
 
 export async function testLinecontact(){
-    var hi =  await retrieveContacts('Spg0nt3GILwAxHbVVKlxoKoMm',"Insurance");
+    var hi =  await retrieveContacts('OD9JjrVluF4ON00310kMWXJLa',"Insurance");
     var items = hi["items"]
     var loop = ""
     var dictCustom = []
